3018c42be9bd733e39f8213702e89d8353184d18
Atharvkote <atharvkote3@gmail.com>
2026-08-08 16:08:08 +0530

    feat(initial):initial setup

---
+++ src/test/java/net/spring_boot/redis_caching/RedisCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.spring_boot.redis_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
